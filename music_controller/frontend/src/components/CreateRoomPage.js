0e2e23d88acac132898119fbdefa18ef84f9bad2
Jemima <jemima_fay@hotmail.co.uk>
2024-01-19 10:33:59 +0000

    handling post requests

--- music_controller/frontend/src/components/CreateRoomPage.js
+++ music_controller/frontend/src/components/CreateRoomPage.js
@@ -1,13 +1,20 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import Button from "@mui/material/Button";
+import { Grid } from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
+import { FormHelperText } from "@mui/material/FormHelperText";
+import FormControlContext from "@mui/material/FormControl/FormControlContext";
+import { Radio } from "@mui/material/Radio";
+import { FormControlLabel } from "@mui/material/FormControlLabel";
+import { Link } from "react-dom";
 
+export default class CreateRoomPage extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-export default class CreateRoomPage extends Component{
-    constructor(props){
-        super(props)
-    }
-
-
-render(){
-    return <p>This is the Create Room Page</p>
-}
+  render() {
+    return <p>This is the Create Room Page</p>;
+  }
 }
